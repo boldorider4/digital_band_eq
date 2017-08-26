d4a4a344d6c4da005e03d0e313bd481b1224f4f1
Jack Olivieri <boldorider4@gmail.com>
2017-08-26 22:04:05 +0200

    test: Handle stereo channels in test main

--- test/python/test_audioclip.py
+++ test/python/test_audioclip.py
@@ -14,26 +14,32 @@ def main():
   # read input audio
   input_filename = 'audio_sample_rock'
   FS, audio_input = ReadWav('../audio_samples/'+input_filename+'.wav')
-  # extract one channel
-  audio_input = audio_input[:,0]
+  # extract channels
+  audio_input_ch0, audio_input_ch1 = audio_input[:,0], audio_input[:,1]
   # pre-allocate output
   audio_output = np.zeros_like(audio_input)
 
-  eq = digitalEq(FS)
-  err = eq.init_eq(F0, Q, G)
+  eq_ch0 = digitalEq(FS)
+  eq_ch1 = digitalEq(FS)
+  err = eq_ch0.init_eq(F0, Q, G)
+  if err:
+    return -1
+  err = eq_ch1.init_eq(F0, Q, G)
   if err:
     return -1
 
   print("FS {}".format(FS))
   print("F0 {}".format(F0))
   print("Q {}".format(Q))
+  print("number of channels {}".format(audio_input[0].size))
   print("type of audio input is {}".format(type(audio_input[0])))
   print("audio is {} samples long".format(len(audio_input)))
   print("rms audio input {}".format(np.sqrt(np.mean(audio_input.astype(np.float)**2))))
   idx = 0
 
-  for sample in audio_input:
-    audio_output[idx] = eq.process_sample(sample)
+  for sample_ch0, sample_ch1 in zip(audio_input_ch0, audio_input_ch1):
+    audio_output[idx, 0] = eq_ch0.process_sample(sample_ch0)
+    audio_output[idx, 1] = eq_ch1.process_sample(sample_ch1)
     idx += 1
 
   output_filename = input_filename+'_filt'
